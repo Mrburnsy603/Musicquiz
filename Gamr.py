90bfbae7122329959d63619aea3e024537ff7eec
Mrburnsy603 <43174183+Mrburnsy603@users.noreply.github.com>
2018-09-29 23:03:06 +0100

    Add files via upload

---
+++ Gamr.py
@@ -0,0 +1,47 @@
+#Username and password
+username = 'User'
+password = 'Password123'
+
+userInput = input("What is your username?\n")
+
+if userInput == username:
+    userInput = input("Password?\n")   
+    if userInput == password:
+       print("Welcome!")
+    else:
+       print("That is the wrong password.")
+       quit()
+else:
+    print("That is the wrong username.")
+    quit()
+Play = input('Are You Ready To Play?')
+if Play == ("Play"):
+  print("Ready Player One")
+else:
+  print("bye")
+print("GO")
+print("green day B________ O_ B______ D_____")
+Song = input("What song is this?")
+if Song == ("Boulevard Of Broken Dreams"):
+  print("yay")
+else:
+  print("oh, better luck next time")
+  quit
+print("Round two")
+print("Dua Lipa N__ R____")
+Song = input("What song is this?")
+if Song == ("New Rules"):
+  print("yay")
+else:
+  print("oh, better luck next time")
+print("Round Three")
+print("Imagine Dragons T______")
+Song = input("What song is this?")
+if Song == ("Thunder"):
+  print("yay")
+else:
+  print("oh, better luck next time")
+ 
+
+
+
